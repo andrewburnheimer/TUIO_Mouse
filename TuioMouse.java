9baa3ad68f0ddd38eab62f25d54531f8d8c954fa
Andrew Burnheimer <Andrew.Burnheimer@nbcuni.com>
2014-11-19 20:48:43 -0500

    Add logging and mutex to disable multi-touch

--- TuioMouse.java
+++ TuioMouse.java
@@ -23,61 +23,96 @@
 import java.awt.*;
 import java.awt.event.*;
 import TUIO.*;
+import java.util.Date;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class TuioMouse implements TuioListener {
-	
+	private static final long LOCK_EXPIRY = 10 * 1000;
+
 	private Robot robot = null;
 	private int width = 0;
 	private int height = 0;
 	private long mouse = -1;
+	private Date locked;
+	private Logger logger;
 
 	public void addTuioObject(TuioObject tobj) {}
-	public void updateTuioObject(TuioObject tobj) {}	
+	public void updateTuioObject(TuioObject tobj) {}
 	public void removeTuioObject(TuioObject tobj) {}
 	public void refresh(TuioTime bundleTime) {}
-	
+
 	public void addTuioCursor(TuioCursor tcur) {
-		if (mouse<0) {
-			mouse = tcur.getSessionID();
-			if (robot!=null) robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
-			if (robot!=null) robot.mousePress(InputEvent.BUTTON1_MASK);
-		}
-		else
-		{
-			if (robot != null) robot.mouseRelease(InputEvent.BUTTON1_MASK);
-			if (robot != null) robot.mousePress(InputEvent.BUTTON2_MASK);
+		Date now = new Date();
+		// Test if lock is zeroed, or just old
+		if ( now.after( new Date(locked.getTime() + LOCK_EXPIRY) ) ) {
+
+			if ( locked.getTime() != 0 ) // the lock timed-out, report it
+			{
+				logger.warn("TuioCursor lock timed-out and is being reset now");
+			}
+
+			locked = now;
+			// Block is entered on all single-touch events
+			if (mouse<0)
+			{
+				logger.debug("addTuioCursor - Top: " + mouse);
+				mouse = tcur.getSessionID();
+				if (robot!=null) robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
+				if (robot!=null) robot.mousePress(InputEvent.BUTTON1_MASK);
+			}
+			else // Block entered on multi-touch events: once for two fingers
+			     // twice for three fingers ... four times for five fingers.
+			{
+				logger.debug("addTuioCursor - Bottom: " + mouse);
+				if (robot != null) robot.mouseRelease(InputEvent.BUTTON1_MASK);
+			}
+			locked = new Date(0);
 		}
 	}
 
 	public void updateTuioCursor(TuioCursor tcur) {
 		if (mouse==tcur.getSessionID()) {
 			if (robot!=null) robot.mouseMove(tcur.getScreenX(width),tcur.getScreenY(height));
-		} 
+		}
 	}
-	
+
 	public void removeTuioCursor(TuioCursor tcur) {
 		if (mouse==tcur.getSessionID()) {
 			mouse=-1;
 			if (robot!=null) robot.mouseRelease(InputEvent.BUTTON1_MASK);
 		}
-		
+
 	}
-	
+
 	public TuioMouse() {
-		try { robot = new Robot(); }
-		catch (Exception e) {
-			System.out.println("failed to initialize mouse robot");
-			System.exit(0);
+		PropertyConfigurator.configureAndWatch("log4jna_tuio.properties");
+		logger = LogManager.getLogger(TuioMouse.class);
+		try {
+			logger.info("TuioMouse process started...");
+		} catch (Throwable t) {
+			System.out.println("Failed to report event to OS");
+			t.printStackTrace();
 		}
 		
+		try { robot = new Robot(); }
+		catch (Exception e) {
+			String message = "failed to initialize mouse robot";
+			logger.fatal(message);
+			System.out.println(message);
+			System.exit(0);
+		}
+
 		width  = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 		height = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+		locked = new Date(0);
 	}
 
 	public static void main(String argv[]) {
-	
+
 		int port = 3333;
- 
+
 		if (argv.length==1) {
 			try { port = Integer.parseInt(argv[1]); }
 			catch (Exception e) { System.out.println("usage: java TuioMouse [port]"); }
